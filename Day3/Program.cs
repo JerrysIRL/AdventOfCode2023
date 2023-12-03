da5999e1c95ffdc5c6cc98a68be33e7e5789f9b8
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-12-03 22:23:02 +0100

    Finished day2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,17 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day3
 {
     internal class Program
     {
         static readonly string[] rawInput = File.ReadAllLines(@"E:\AdventOfCode\AdventOfCode2023\Day3\Input.txt");
-        static readonly List<char> symbols = new List<char>() { '*', '/', '&', '@', '%', '+', '=', '#', '$','-'};
-        
+        static readonly List<char> symbols = new List<char>() { '*', '/', '&', '@', '%', '+', '=', '#', '$', '-' };
+
         public static void Main(string[] args)
         {
             List<PartNumber> numbersList = new List<PartNumber>();
+            PartOne(numbersList);
+
+            int sumGearRatios = 0;
+
+            foreach (var gearCoord in GetGearsCoordinates())
+            {
+                int gearRatio = CalculateGearRatio(gearCoord.Item1, gearCoord.Item2);
+                sumGearRatios += gearRatio;
+            }
+
+            Console.WriteLine(sumGearRatios);
+
+            List<(int, int)> GetGearsCoordinates()
+            {
+                List<(int, int)> gears = new List<(int, int)>();
+
+                for (int y = 0; y < rawInput.Length; y++)
+                {
+                    var line = rawInput[y];
+                    for (int x = 0; x < line.Length; x++)
+                    {
+                        char c = line[x];
+                        if (c == '*')
+                        {
+                            int adjacentPartNumbers = GetAdjacentPartNumbers(y, x).Count;
+                            if (adjacentPartNumbers == 2)
+                            {
+                                gears.Add((y, x));
+                            }
+                        }
+                    }
+                }
+
+                return gears;
+            }
+
+            int CalculateGearRatio(int y, int x)
+            {
+                var adjacentPartNumbers = GetAdjacentPartNumbers(y, x);
+                if (adjacentPartNumbers.Count == 2)
+                {
+                    int ratio = Int32.Parse(adjacentPartNumbers[0].Number) * Int32.Parse(adjacentPartNumbers[1].Number);
+                    return ratio;
+                }
+                return 0;
+            }
+
+            List<PartNumber> GetAdjacentPartNumbers(int y, int x)
+            {
+                HashSet<PartNumber> connectingPartNumbers = new HashSet<PartNumber>();
+
+                for (int i = -1; i <= 1; i++)
+                {
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        int newY = y + i;
+                        int newX = x + j;
+
+                        // Skip the current cell
+                        if (i == 0 && j == 0)
+                        {
+                            continue;
+                        }
+
+                        // Skip invalid coordinates
+                        if (newY < 0 || newY >= rawInput.Length || newX < 0 || newX >= rawInput[newY].Length)
+                        {
+                            continue;
+                        }
+
+                        char c = GetCharAtPosition(newY, newX);
+
+                        if (char.IsDigit(c))
+                        {
+                            connectingPartNumbers.Add(numbersList.Find(num => num.CoordinatesList.Contains((newY, newX))));
+                        }
+                    }
+                }
+
+                return connectingPartNumbers.ToList();
+            }
+            
+            
+            int sum = 0;
+            numbersList.ForEach(s =>
+            {
+                s.Valid = s.IsValidPartNumber(s);
+                if (s.Valid)
+                {
+                    sum += Int32.Parse(s.Number);
+                }
+            });
+            Console.WriteLine(sum);
+        }
+
+        private static void PartOne(List<PartNumber> numbersList)
+        {
             for (int y = 0; y < rawInput.Length; y++)
             {
                 var line = rawInput[y];
@@ -30,63 +128,15 @@ namespace Day3
                         }
                     }
                 }
-                numbersList.Add(partNumber);
-            }
 
-
-            int sum = 0;
-            numbersList.ForEach(s =>
-            {
-                s.Valid = IsValidPartNumber(s);
-                if (s.Valid)
+                if (partNumber.Number != "")
                 {
-                    sum += Int32.Parse(s.Number);
+                    numbersList.Add(partNumber);
                 }
-            });
-            Console.WriteLine(sum);
-
-            
-
-            bool IsValidPartNumber(PartNumber partNumber)
-            {
-                foreach (var coord in partNumber.CoordinatesList)
-                {
-                    int y = coord.Item1;
-                    int x = coord.Item2;
-                    
-                    for (int i = -1; i <= 1; i++)
-                    {
-                        for (int j = -1; j <= 1; j++)
-                        {
-                            int newY = y + i;
-                            int newX = x + j;
-                            
-                            if (i == 0 && j == 0)
-                            {
-                                continue;
-                            }
-                            
-                            if (newY < 0 || newY >= rawInput.Length || newX < 0 || newX >= rawInput[newY].Length)
-                            {
-                                continue;
-                            }
-
-                            char c = GetCharFromInput(newY, newX);
-
-                            if (symbols.Contains(c))
-                            {
-                                return true;
-                            }
-                        }
-                    }
-                }
-
-                return false;
             }
         }
 
-
-        public class PartNumber
+        private class PartNumber
         {
             public string Number { get; set; }
             public List<(int, int)> CoordinatesList { get; set; }
@@ -102,9 +152,46 @@ namespace Day3
             {
                 CoordinatesList.Add(coordinates);
             }
+
+
+            public bool IsValidPartNumber(PartNumber partNumber)
+            {
+                foreach (var coord in partNumber.CoordinatesList)
+                {
+                    int y = coord.Item1;
+                    int x = coord.Item2;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int newY = y + i;
+                            int newX = x + j;
+
+                            if (i == 0 && j == 0)
+                            {
+                                continue;
+                            }
+
+                            if (newY < 0 || newY >= rawInput.Length || newX < 0 || newX >= rawInput[newY].Length)
+                            {
+                                continue;
+                            }
+
+                            char c = GetCharAtPosition(newY, newX);
+                            if (symbols.Contains(c))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+
+                return false;
+            }
         }
 
-        static char GetCharFromInput(int y, int x)
+        static char GetCharAtPosition(int y, int x)
         {
             return rawInput[y][x];
         }
