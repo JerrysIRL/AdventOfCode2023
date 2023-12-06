859af7489357bb9171a162857815e666faea6042
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-12-06 10:18:38 +0100

    Day 6 part1

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace Day6
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6
 {
     internal class Program
     {
+        static readonly int[] Time = new[] { 45, 98, 83, 73 };
+        static readonly int[] Distance = new[] { 295, 1734, 1278, 1210 };
+
+
         public static void Main(string[] args)
         {
+            List<int> recordList = new List<int>();
+            P1(recordList);
+            
+            int p1 = recordList.Aggregate((x, y) => x * y);
+            Console.WriteLine(p1);
         }
+
+        private static void P1(List<int> recordList)
+        {
+            for (int i = 0; i < Time.Length; i++)
+            {
+                int recordCount = 0;
+                for (int j = 0; j < Time[i]; j++)
+                {
+                    var remainingTime = Time[i] - j;
+                    if (j * remainingTime > Distance[i])
+                        recordCount++;
+                }
+                recordList.Add(recordCount);
+            }
+        }
+        
     }
 }
