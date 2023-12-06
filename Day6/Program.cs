d6cf3ddc0c9d52b0f22e2eb1d26f68a719e6f059
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-12-06 10:19:26 +0100

    Part2 solution

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -8,15 +8,19 @@ namespace Day6
     {
         static readonly int[] Time = new[] { 45, 98, 83, 73 };
         static readonly int[] Distance = new[] { 295, 1734, 1278, 1210 };
+        static readonly int TimeP2 = 45988373;
+        static readonly long DistanceP2 = 295173412781210;
 
 
         public static void Main(string[] args)
         {
             List<int> recordList = new List<int>();
             P1(recordList);
-            
             int p1 = recordList.Aggregate((x, y) => x * y);
+            int p2 = P2();
+            
             Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
 
         private static void P1(List<int> recordList)
@@ -33,6 +37,18 @@ namespace Day6
                 recordList.Add(recordCount);
             }
         }
-        
+
+        private static int P2()
+        {
+            int recordCount = 0;
+            for (long j = 0; j < TimeP2; j++)
+            {
+                long remainingTime = TimeP2 - j;
+                if (j * remainingTime > DistanceP2)
+                    recordCount++;
+            }
+
+            return recordCount;
+        }
     }
 }
