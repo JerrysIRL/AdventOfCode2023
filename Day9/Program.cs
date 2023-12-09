047e1089da406dc20bb9fc316f152d460f29800e
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-12-09 10:32:01 +0100

    started Day 9

---
+++ Day9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day9
+{
+    internal class Program
+    {
+        private static readonly string[] Input = File.ReadAllLines(@"E:\AdventOfCode\AdventOfCode2023\Day9\Input.txt");
+        public static void Main(string[] args)
+        {
+            List<List<int>> HistoryList = new List<List<int>>();
+            foreach (var line in Input)
+            {
+                var numberLine = line.Split(' ').ToList().ConvertAll(e => Int32.Parse(e));
+                HistoryList.Add(numberLine);
+            }
+
+           
+            
+        }
+        
+        public int FindDifference(int nr1, int nr2)
+        {
+            return Math.Abs(nr1 - nr2);
+        }
+
+        public class History
+        {
+            public List<int> Numbers = new List<int>();
+            public int Difference { get; set; }
+        }
+        
+        
+    }
+}
