20e3b3f5a5c4c3e7552ab1d123e8cf18a6215ae0
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-12-09 16:06:19 +0100

    Day 9 solution part 1

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -2,36 +2,96 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting;
 
 namespace Day9
 {
     internal class Program
     {
         private static readonly string[] Input = File.ReadAllLines(@"E:\AdventOfCode\AdventOfCode2023\Day9\Input.txt");
+
         public static void Main(string[] args)
         {
-            List<List<int>> HistoryList = new List<List<int>>();
+            List<List<int>> historyList = new List<List<int>>();
             foreach (var line in Input)
             {
-                var numberLine = line.Split(' ').ToList().ConvertAll(e => Int32.Parse(e));
-                HistoryList.Add(numberLine);
+                var numList = line.Split(' ').ToList().ConvertAll(e => Int32.Parse(e));
+                historyList.Add(numList);
             }
 
-           
+            int sum = 0;
             
-        }
-        
-        public int FindDifference(int nr1, int nr2)
-        {
-            return Math.Abs(nr1 - nr2);
+            foreach (var history in historyList)
+            {
+                List<List<int>> tempList = new List<List<int>>();
+                tempList.Add(history);
+                var temp = history;
+                do
+                {
+                    temp = CreateNewRow(temp);
+                    tempList.Add(temp);
+                } while (ReachedZero(tempList));
+                
+                tempList.Reverse();
+                for (int i = 0; i < tempList.Count; i++)
+                {
+                    if (i + 1 >= tempList.Count)
+                    {
+                        
+                        break;
+                    }
+                    int newnum = tempList[i].Last() + tempList[i + 1].Last();
+                    tempList[i+1].Add(newnum);
+                }
+
+                sum += tempList.Last().Last();
+            }
+            
+            Console.WriteLine(sum);
+
         }
 
-        public class History
+        static bool ReachedZero(List<List<int>> input)
         {
-            public List<int> Numbers = new List<int>();
-            public int Difference { get; set; }
+            var boolList = input.Last().Select(n => n == 0);
+            foreach (var b in boolList)
+            {
+                if (!b)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
-        
-        
+
+
+        static List<int> CreateNewRow(List<int> history)
+        {
+            List<int> newRow = new List<int>();
+            for (int i = 0; i < history.Count; i++)
+            {
+                if (i + 1 >= history.Count)
+                {
+                    break;
+                }
+
+                var result = history[i + 1] - history[i];
+                newRow.Add(result);
+            }
+
+            return newRow;
+        }
+
+        static int FindDifference(int nr1, int nr2)
+        {
+            return nr1 - nr2;
+        }
+
+        // public class History
+        // {
+        //     public List<List<int>> Numbers = new List<List<int>>();
+        //     //public int Difference { get; set; }
+        // }
     }
 }
