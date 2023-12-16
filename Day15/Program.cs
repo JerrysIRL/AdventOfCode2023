6777ed75b50c5e22d794d4ce93f4c7a3cd9dd996
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-12-16 11:27:44 +0100

    day 15 part 1 solution

---
+++ Day15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day15
+{
+    internal class Program
+    {
+        static readonly string Input = File.ReadAllText(@"E:\AdventOfCode\AdventOfCode2023\Day15\Input.txt");
+
+        public static void Main(string[] args)
+        {
+            var sections = Input.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+            var sectionValues = new List<int>();
+            foreach (var s in sections)
+            {
+                int currentValue = 0;
+                foreach (var c in s)
+                {
+                    var value = (int)c;
+                    value += currentValue;
+                    value *= 17;
+                    value %= 256;
+                    currentValue = value;
+                }
+
+                sectionValues.Add(currentValue);
+            }
+
+            var p1 = sectionValues.Sum(i => i);
+            Console.WriteLine(p1);
+        }
+    }
+}
